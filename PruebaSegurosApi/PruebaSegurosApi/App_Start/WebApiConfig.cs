e1a10dcf0b318e707045d5629e8b225efaea784a
Ricardo-PC\Ricardo <rosoriog@psl.com.co>
2019-02-24 20:45:29 -0500

    Activacion de peticionesde origenes cruzados

--- PruebaSegurosApi/PruebaSegurosApi/App_Start/WebApiConfig.cs
+++ PruebaSegurosApi/PruebaSegurosApi/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace PruebaSegurosApi
 {
@@ -11,7 +12,8 @@ namespace PruebaSegurosApi
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
-
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Rutas de API web
             config.MapHttpAttributeRoutes();
 
